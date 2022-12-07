9b1ab26b120ebfa9224db2366735bb9dedc94010
JevgenijKlopov <jevgenijklopov@gmail.com>
2022-12-07 21:24:42 +0200

    Task 4 Finish

--- JS Tasks/Task 4/script.js
+++ JS Tasks/Task 4/script.js
@@ -9,3 +9,13 @@ būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+import cars from './cars.json' assert {type: 'json'}
+console.log(cars)
+const result = document.querySelector("#output")
+cars.forEach(item =>{
+    let modelItems = ""
+    item.models.forEach(item=>{
+        modelItems += `<span>${item}</span>, `
+    })
+    result.innerHTML +=`<div class="card"><h1>${item.brand}</h1><p>${modelItems}</p></div>`
+})
